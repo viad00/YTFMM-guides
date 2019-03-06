c10e75b02f55b87206899be9451ace931bb0237d
Vlad Utkin <vlad@teamfnd.ru>
2019-03-07 00:49:21 +0300

    Deli2 Fix

--- YTFMM/views.py
+++ YTFMM/views.py
@@ -103,7 +103,7 @@ def yandex_callback(request):
         # notification_type&operation_id&amount&currency&datetime&sender&codepro&notification_secret&label
         m = hashlib.sha1('{}&{}&{}&{}&{}&{}&{}&{}&{}'.format(notification_type, operation_id, amount, currency, datetime,
                                                              sender, codepro, notification_secret, label))
-        if m.hexdigest() == request.POST['sha1_hash'][0]:
+        if m.hexdigest() == request.POST['sha1_hash']:
             or_id = int(label[2:])
             s = Order.objects.get(id=or_id)
             if s.value_to_pay == float(amount):
