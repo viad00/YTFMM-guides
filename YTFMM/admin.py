128c347c7e2fdbbc9a8f18d6f8b557c5b7486f2b
Vlad Utkin <vlad@teamfnd.ru>
2019-03-06 23:05:51 +0300

    Add models admin (simple)

--- YTFMM/admin.py
+++ YTFMM/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, Setting
 
 # Register your models here.
+
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value')
+
+
+admin.site.register(Setting, SettingAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_id', 'value_to_pay', 'payment_type', 'paid', 'created')
+
+
+admin.site.register(Order, OrderAdmin)
